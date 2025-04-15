1c6001d3f82648920dbc5c7a5e932fc5bee7d9f9
Helena Flausino <helenaflausino@gmail.com>
2025-04-15 16:31:23 -0300

    A

--- JogaJogoDaVelha.java
+++ JogaJogoDaVelha.java
@@ -2,38 +2,52 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class JogaJogoDaVelha {
+
     public static void main(String[] args) {
-        System.out.println("Bem-vindo ao teste do Jogo da Velha!");
         Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        System.out.println("Bem-vindo ao teste do Jogo da Velha!");
         System.out.println("Digite 1 para jogar ou 0 para sair: ");
         int opcao = scanner.nextInt();
-        Random random = new Random();
         while (opcao!=0) {
-            System.out.println("Digite um número inteiro para a dimensão do tabuleiro: ");
-            int dimensao = scanner.nextInt();
-            JogoDaVelha jogo = new JogoDaVelha(dimensao);
-            int i, j;
-            while (jogo.vencedor() == 2) {
-                i = random.nextInt(3);
-                j = random.nextInt(3);
-                try {
-                    jogo.poePeca(i, j);
-                } catch (IllegalArgumentException e) {
-                    System.out.println(e.getMessage());
-                    continue;
-                }
-//                System.out.println(jogo.toString());
+            
+            int tamanho;
+  
+            System.out.print("Digite o tamanho do tabuleiro ");
+            tamanho = scanner.nextInt();
+                
+
+            JogoDaVelha jogo = new JogoDaVelha(tamanho);
+
+            while (jogo.vencedor() == 0 && !empate(jogo)) {
+                int linha, coluna;
+                do {
+                    linha = random.nextInt(tamanho);
+                    coluna = random.nextInt(tamanho);
+                } while (jogo.tabuleiro[linha][coluna] != JogoDaVelha.VAZIO);
+
+                jogo.poePeca(linha, coluna);
+               
             }
-            if (jogo.vencedor() == 1) {
-                System.out.println("Jogador X venceu!");
-            } else if (jogo.vencedor() == -1) {
-                System.out.println("Jogador O venceu!");
-            } else {
-                System.out.println("Empate!");
-            }
-            System.out.println(jogo.toString());
+
+            System.out.println(jogo);
+
+            int vencedor = jogo.vencedor();
+            if (vencedor == 1) System.out.println("1");
+            else if (vencedor == -1) System.out.println("-1");
+            else System.out.println("0");
+
             System.out.println("Deseja jogar novamente? (1-sim, 0-nao): ");
             opcao = scanner.nextInt();
-        }
+        } 
+    }
+    
+    public static boolean empate(JogoDaVelha jogo) {
+        for (int i = 0; i < jogo.tabuleiro.length; i++)
+            for (int j = 0; j < jogo.tabuleiro.length; j++)
+                if (jogo.tabuleiro[i][j] == JogoDaVelha.VAZIO)
+                    return false;
+        return jogo.vencedor() == 0;
     }
 }
--- JogoDaVelha.java
+++ JogoDaVelha.java
@@ -1,66 +1,82 @@
 public class JogoDaVelha {
     protected static final int X = 1, O = -1;
     protected static final int VAZIO = 0;
-    protected int tabuleiro[][] = new int[3][3];
+    protected int[][] tabuleiro;
     protected int jogador;
+    protected int tamanho;
 
-    public JogoDaVelha() {
+    public JogoDaVelha(int tamanho) {
+        this.tamanho = tamanho;
+        tabuleiro = new int[tamanho][tamanho];
         limpaTabuleiro();
     }
 
     public void limpaTabuleiro() {
-        for(int i = 0;i<3;i++) {
-            for (int j=0; j<3; j++) {
-                tabuleiro[i][j]=VAZIO;
-            }
-        }
+        for (int i = 0; i < tamanho; i++)
+            for (int j = 0; j < tamanho; j++)
+                tabuleiro[i][j] = VAZIO;
         jogador = X;
     }
 
     public void poePeca(int i, int j) {
-        if (i<0||i>2||j<0||j>2){
+        if (i < 0 || i >= tamanho || j < 0 || j >= tamanho)
             throw new IllegalArgumentException("Posição Inválida");
-        }
-        if (tabuleiro[i][j]!=VAZIO) throw new IllegalArgumentException("Posição Ocupada");
-        tabuleiro[i][j]=jogador;
+        if (tabuleiro[i][j] != VAZIO)
+            throw new IllegalArgumentException("Posição Ocupada");
+        tabuleiro[i][j] = jogador;
         jogador = -jogador;
     }
 
-
     public int vencedor() {
-        int resultado = 2;
-        // Implemente este método que deve retornar o vencedor ou 
-        // zero em caso de empate e 2 se o jogo não acabou.
+        for (int i = 0; i < tamanho; i++) {
+            int somaLinha = 0;
+            int somaColuna = 0;
+            for (int j = 0; j < tamanho; j++) {
+                somaLinha += tabuleiro[i][j];
+                somaColuna += tabuleiro[j][i];
+            }
+            if (somaLinha == X * tamanho || somaColuna == X * tamanho)
+                return 1;
+            if (somaLinha == O * tamanho || somaColuna == O * tamanho)
+                return -1;
+        }
 
-        return resultado;
+
+        int diagonalPrincipal = 0;
+        int diagonalSecundaria = 0;
+        for (int i = 0; i < tamanho; i++) {
+            diagonalPrincipal += tabuleiro[i][i];
+            diagonalSecundaria += tabuleiro[i][tamanho - 1 - i];
+        }
+        if (diagonalPrincipal == X * tamanho || diagonalSecundaria == X * tamanho)
+            return 1;
+        if (diagonalPrincipal == O * tamanho || diagonalSecundaria == O * tamanho)
+            return -1;
+
+        return 0; 
     }
 
-    
+    @Override
     public String toString() {
-        /** Implementar o método to String que deve retornar
-         * uma string com o tabuleiro do jogo da velha com as peças
-         * nas posições corretas.
-         */
-        String retorno = "";
-        for (int i=0; i<3;i++){
-            for (int j=0; j<3; j++){
-                if(tabuleiro[i][j]==X) {
-                    retorno += ("X");
-                } else if (tabuleiro[i][j]==O) {
-                    retorno += ("O");
-                } else {
-                    retorno += (" ");
+        StringBuilder sb = new StringBuilder(); // usei esse string builder porque é melhor pra mostrar o resultado
+        for (int i = 0; i < tamanho; i++) {
+            for (int j = 0; j < tamanho; j++) {
+                switch (tabuleiro[i][j]) {
+                    case X: sb.append(" X "); break;
+                    case O: sb.append(" O "); break;
+                    default: sb.append("   ");
                 }
-                if (j<2){
-                    retorno += ("|");
+                if (j < tamanho - 1) sb.append("|");
+            }
+            sb.append("\n");
+            if (i < tamanho - 1) {
+                for (int k = 0; k < tamanho; k++) {
+                    sb.append("---");
+                    if (k < tamanho - 1) sb.append("+");
                 }
+                sb.append("\n");
             }
-            //System.out.println();
-            if (i<2){
-                retorno += ("\n-----\n");
-            }
-
-        }   
-        return retorno;
+        }
+        return sb.toString();
     }
 }
